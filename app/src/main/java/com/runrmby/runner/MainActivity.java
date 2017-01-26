7673aff4ed3a170a77de5f2ad3a26cec3496134d
Nomadjackalope <bendoo02@gmail.com>
2017-01-26 09:58:22 -0800

    Commented out toggling on touch

--- app/src/main/java/com/runrmby/runner/MainActivity.java
+++ app/src/main/java/com/runrmby/runner/MainActivity.java
@@ -98,7 +98,7 @@ public class MainActivity extends AppCompatActivity {
         mContentView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                toggle();
+                //toggle();
             }
         });
 
